f6d183cf05b6966f8cff2b0720c37fe2b5f1f21b
Lee <why48382@gmail.com>
2025-08-14 13:48:55 +0900

    [fix] @EntityListeners(AuditingEntityListener.class)추가

--- src/main/java/org/example/coding_convention/file/model/Files.java
+++ src/main/java/org/example/coding_convention/file/model/Files.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import org.example.coding_convention.project.model.Project;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Files {
 
     @Id
